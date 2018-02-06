fa71cf038f664821b96dc33106dbae79d6b9180d
Bharath Upadhya <1992bharath@gmail.com>
2018-02-06 16:11:39 +0530

    use git archive to create tar.gz

--- buildAndPushArchives.sh
+++ buildAndPushArchives.sh
@@ -6,25 +6,21 @@ export VERSION=master
 
 set_context() {
   export RES_REPO="$CONTEXT_repo"
-  export ARTIFACT_TMP_DIR="/tmp/$CONTEXT"
   export ARTIFACT_TAR="/tmp/$CONTEXT-$VERSION.tar.gz"
   export S3_BUCKET_DIR="$ARTIFACTS_BUCKET/$CONTEXT/$VERSION/"
   export ARTIFACT_SRC_DIR=$(shipctl get_resource_state $RES_REPO)
 
   echo "export RES_REPO=$CONTEXT_repo"
-  echo "export ARTIFACT_TMP_DIR=$ARTIFACT_TMP_DIR"
   echo "export ARTIFACT_TAR=$ARTIFACT_TAR"
   echo "export S3_BUCKET_DIR=$S3_BUCKET_DIR"
   echo "export ARTIFACT_SRC_DIR=$ARTIFACT_SRC_DIR"
 }
 
 create_tar() {
-  pushd /tmp
+  pushd $RES_REPO
   echo "Creating tar $ARTIFACT_TAR..."
-  rm -rf $ARTIFACT_TMP_DIR
   rm -rf $ARTIFACT_TAR
-  cp -rf $ARTIFACT_SRC_DIR $ARTIFACT_TMP_DIR
-  tar -zcf "$ARTIFACT_TAR" -C "$ARTIFACT_TMP_DIR" .
+  git archive --format=tar.gz --output=$ARTIFACT_TAR --prefix=$CONTEXT/ $VERSION
   echo "Successfully created $ARTIFACT_TAR"
   popd
 }
